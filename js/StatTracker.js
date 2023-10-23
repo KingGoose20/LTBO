618ecabdfed2b655b72375d881717d050718ac91
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2023-10-23 11:20:20 +1000

    Update StatTracker.js

--- js/StatTracker.js
+++ js/StatTracker.js
@@ -8,7 +8,11 @@ Mstreak = 0
 Sstreak = 1
 pastScorer = ""
 
+ChocTops.Players.push("Choc-Tops OTHER")
+GentleMen.Players.push("Gentle, Men OTHER")
+TrafficControllers.Players.push("Traffic Controllers OTHER")
 
+console.log(ChocTops.Players[5])
 function start() {
     aElement = document.getElementById("aTeam")
     bElement = document.getElementById("bTeam")
