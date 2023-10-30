649dcf39c402ea7656594dfaefd8c4581249bd11
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2023-10-30 12:52:36 +1000

    trades

--- js/Array.js
+++ js/Array.js
@@ -1,15 +1,15 @@
 mainArray = {
 
   "Name": ["Jasper Collier", "Conor Farrington", "Alexander Galt", "Rudy Hoschke", "Michael Iffland", "Lukas Johnston", "Sam James", "Clarrie Jones", "William Kim", "Samuel McConaghy", "Ryan Pattemore", "William Scott", "Nicholas Szogi", "Christopher Tomkinson", "Angus Walker", "Will Weekes"],
-  "Team": ["Traffic Controllers", "Gentle, Men", "Gentle, Men", "Gentle, Men", "Choc-Tops", "Gentle, Men", "Choc-Tops", "Traffic Controllers", "Traffic Controllers", "Traffic Controllers", "Choc-Tops", "", "Choc-Tops", "Traffic Controllers", "Choc-Tops", "Gentle, Men"],
-  "PPG":[0,2.33,4,4.67,1.33,0.33,1,1.67,1.67,2,0.67,0,0.33,2,0.67,0.67],
-  "TP":[0,7,4,14,4,1,3,5,5,6,2,0,1,6,2,2],
-  "FPG":[0,0.67,4,3.67,0.33,0,0.67,0.67,0.67,0,0,0,0.33,1.67,0,0.67],
-  "TF":[0,2,4,11,1,0,2,2,2,0,0,0,1,5,0,2],
-  "MPG":[0,1.67,0,0.33,0.33,0.33,0.33,0.33,0.33,2,0.67,0,0,0.33,0,0],
-  "TM":[0,5,0,1,1,1,1,1,1,6,2,0,0,1,0,0],
-  "TPG":[0,0,0,0.33,0.33,0,0,0.33,0.33,0,0,0,0,0,0.33,0],
-  "TT":[0,0,0,1,1,0,0,1,1,0,0,0,0,0,1,0],  
+  "Team": ["Gentle, Men", "Gentle, Men", "Gentle, Men", "Gentle, Men", "Choc-Tops", "Traffic Controllers", "Choc-Tops", "Traffic Controllers", "Traffic Controllers", "Traffic Controllers", "Choc-Tops", "", "Choc-Tops", "Gentle, Men", "Choc-Tops", "Traffic Controllers"],
+  "PPG": [0, 2.33, 4, 4.67, 1.33, 0.33, 1, 1.67, 1.67, 2, 0.67, 0, 0.33, 2, 0.67, 0.67],
+  "TP": [0, 7, 4, 14, 4, 1, 3, 5, 5, 6, 2, 0, 1, 6, 2, 2],
+  "FPG": [0, 0.67, 4, 3.67, 0.33, 0, 0.67, 0.67, 0.67, 0, 0, 0, 0.33, 1.67, 0, 0.67],
+  "TF": [0, 2, 4, 11, 1, 0, 2, 2, 2, 0, 0, 0, 1, 5, 0, 2],
+  "MPG": [0, 1.67, 0, 0.33, 0.33, 0.33, 0.33, 0.33, 0.33, 2, 0.67, 0, 0, 0.33, 0, 0],
+  "TM": [0, 5, 0, 1, 1, 1, 1, 1, 1, 6, 2, 0, 0, 1, 0, 0],
+  "TPG": [0, 0, 0, 0.33, 0.33, 0, 0, 0.33, 0.33, 0, 0, 0, 0, 0, 0.33, 0],
+  "TT": [0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0],
   "Missed": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   "AccoladesOne": ["", "", "All-Defence Team T1", "All-Offence Team T1", "Playmaker T1", "MVP Runner Up T1", "Miles Morales", "LTBO Manager", "MVP T1", "GM", "Perimeter T1", "LTBO Photographer", "The Biggest Bird", "MIP T1", "LTBO CEO", "Teammate T1"],
   "AccoladesTwo": ["", "", "Champion T1", "All-Defence Team T1", "Thirdman T1", "All-Offence Team T1", "Champion T2", "Champion T1", "All-Offence Team T1", "All-Offence Team T1", "Champion T1", "", "Champion T1", "MIP T2", "GM", "Champion T1"],
@@ -19,8 +19,8 @@ mainArray = {
   "AccoladesSix": ["", "", "", "Scoring Champ T2", "", "", "", "All-2nd-Defence Team T2", "Champion T2", "", "", "", "All-2nd-Defence Team T2", "Champion T2", "All-Offence Team T2", ""],
   "AccoladesSeven": ["", "", "", "GM", "", "", "", "Champion T2", "Finals MVP T2", "", "", "", "", "", "All-Defence Team T2", ""],
   "AccoladesEight": ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "Champion T2", ""],
-  "History": ["Drafted by Choc-Tops. Traded to Traffic Controllers.", "GM of Gentle, Men", "Drafted by Traffic Controllers. Traded to Gentle, Men.", "Drafted by Gentle, Men", "Drafted by Gentle, Men. Traded to Choc-Tops.", "Drafted by Gentle, Men. Traded to Choc-Tops. Traded to Traffic Controllers. Traded to Gentle, Men", "Drafted by Traffic Controllers. Traded to Choc-Tops.", "GM of Traffic Controllers", "Drafted by Traffic Controllers. Traded to Choc-Tops. Traded to Traffic Controllers.", "Drafted by Traffic Controllers", "GM of Choc-Tops", "Unsigned", "Drafted by Choc-Tops. Traded to Traffic Controllers. Traded to Choc-Tops", "Drafted by Choc-Tops. Traded to Gentle, Men. Traded to Traffic Controllers.", "Drafted by Choc-Tops", "Drafted by Gentle, Men"],
-  "TeamImage": ["../Images/TC_Final.png", "../Images/GM_Final.png", "../Images/GM_Final.png", "../Images/GM_Final.png", "../Images/CT_Final.png", "../Images/GM_Final.png", "../Images/CT_Final.png", "../Images/TC_Final.png", "../Images/TC_Final.png", "../Images/TC_Final.png", "../Images/CT_Final.png", "../Images/Logo.png", "../Images/CT_Final.png", "../Images/TC_Final.png", "../Images/CT_Final.png", "../Images/GM_Final.png"],
+  "History": ["Drafted by Choc-Tops. Traded to Traffic Controllers. Traded to Gentle-Men", "GM of Gentle, Men", "Drafted by Traffic Controllers. Traded to Gentle, Men.", "Drafted by Gentle, Men", "Drafted by Gentle, Men. Traded to Choc-Tops.", "Drafted by Gentle, Men. Traded to Choc-Tops. Traded to Traffic Controllers. Traded to Gentle, Men. Traded to Traffic Controllers", "Drafted by Traffic Controllers. Traded to Choc-Tops.", "GM of Traffic Controllers", "Drafted by Traffic Controllers. Traded to Choc-Tops. Traded to Traffic Controllers.", "Drafted by Traffic Controllers", "GM of Choc-Tops", "Unsigned", "Drafted by Choc-Tops. Traded to Traffic Controllers. Traded to Choc-Tops", "Drafted by Choc-Tops. Traded to Gentle, Men. Traded to Traffic Controllers. Traded to Gentle, Men.", "Drafted by Choc-Tops", "Drafted by Gentle, Men. Traded to Traffic Controllers"],
+  "TeamImage": ["../Images/GM_Final.png", "../Images/GM_Final.png", "../Images/GM_Final.png", "../Images/GM_Final.png", "../Images/CT_Final.png", "../Images/TC_Final.png", "../Images/CT_Final.png", "../Images/TC_Final.png", "../Images/TC_Final.png", "../Images/TC_Final.png", "../Images/CT_Final.png", "../Images/Logo.png", "../Images/CT_Final.png", "../Images/GM_Final.png", "../Images/CT_Final.png", "../Images/TC_Final.png"],
   "PlayerImage": ["../Images/Players/Jasper.png", "../Images/Players/Conor.png", "../Images/Players/Alex.png", "../Images/Players/Rudy.png", "../Images/Players/Michael.png", "../Images/Players/Lukas.png", "../Images/Players/SamJ.png", "../Images/Players/Clarrie.png", "../Images/Players/Kimmy.png", "../Images/Players/SamM.png", "../Images/Players/Ryan.png", "../Images/Players/Will.png", "../Images/Players/Nicholas.png", "../Images/Players/Christopher.png", "../Images/Players/Angus.png", "../Images/Players/Will.png"],
 
 }
@@ -42,13 +42,13 @@ dayArray = [
     "ThreePointers": [0, 0, "Did not Play", 1, 1, 0, 0, 1, 0, 0, 0, "Did not Play", 0, 0, 0, 0]
   },
   {
-    "Date":["25-October"],
-    "Points":[0,1,4,2,0,0,0,1,3,2,1,"Did not Play",0,2,2,0],
-    "Finishes":[0,0,4,1,0,0,0,1,2,0,0,"Did not Play",0,1,0,0],
-    "Midrange":[0,1,0,1,0,0,0,0,1,2,1,"Did not Play",0,1,0,0],
-    "ThreePointers":[0,0,0,0,0,0,0,0,0,0,0,"Did not Play",0,0,1,0]
-    },
-    
+    "Date": ["25-October"],
+    "Points": [0, 1, 4, 2, 0, 0, 0, 1, 3, 2, 1, "Did not Play", 0, 2, 2, 0],
+    "Finishes": [0, 0, 4, 1, 0, 0, 0, 1, 2, 0, 0, "Did not Play", 0, 1, 0, 0],
+    "Midrange": [0, 1, 0, 1, 0, 0, 0, 0, 1, 2, 1, "Did not Play", 0, 1, 0, 0],
+    "ThreePointers": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "Did not Play", 0, 0, 1, 0]
+  },
+
 
 
 
