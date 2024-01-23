f4c5d6b8a7d2d1d06a47f72d8cd6d8db9c9e4eeb
KingGoose20 <41673777+KingGoose20@users.noreply.github.com>
2024-01-23 10:25:22 +1000

    DNPs

--- js/Array.js
+++ js/Array.js
@@ -1,33 +1,35 @@
 /* -------------- PUT INFO HERE -------------- */
 mainArray = {
-  "Name":["Jasper Collier","Conor Farrington","Alexander Galt","Rudy Hoschke","Michael Iffland","Lukas Johnston","Sam James","Clarrie Jones","William Kim","Samuel McConaghy","Ryan Pattemore","Nicholas Szogi","Christopher Tomkinson","Angus Walker","Will Weekes","Mitch Yue"],
-"Team":["Choc-Tops","Gentle, Men","Choc-Tops","Choc-Tops","Gentle, Men","Gentle, Men","Traffic Controllers","Traffic Controllers","Traffic Controllers","Gentle, Men","Choc-Tops","Traffic Controllers","Choc-Tops","Traffic Controllers","Gentle, Men","Gentle, Men"],
-"PPG":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"0"],
-"TP":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"0"],
-"FPG":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"0"],
-"TF":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"0"],
-"MPG":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"0"],
-"TM":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"0"],
-"TPG":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"0"],
-"TT":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"0"],
-"Missed":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,"0"],
-"AccoladesOne":["","","All-Defence Team T1","All-Offence Team T1","Playmaker T1","MVP Runner Up T1","Miles Morales","LTBO Manager","MVP T1","GM","Perimeter T1","The Biggest Bird","MIP T1","LTBO CEO","Teammate T1",""],
-"AccoladesTwo":["","","Champion T1","All-Defence Team T1","Thirdman T1","All-Offence Team T1","Champion T2","Champion T1","All-Offence Team T1","All-Offence Team T1","Champion T1","Champion T1","MIP T2","GM","Champion T1",""],
-"AccoladesThree":["","","All-Offence Team T2","Champion T1","All-Offence Team T2","All-2nd-Offence Team T2","","Finals MVP T1","All-Defence Team T1","All-Defence Team T1","","MVP Runner Up T2","MVP T2","All-Offence Team T1","Teammate T2",""],
-"AccoladesFour":["","","All-Defence Team T2","All-Offence Team T2","All-2nd-Defence Team T2","","","Fifthman T2","All-2nd-Offence Team T2","All-Offence Team T2","","X-Factor T2","All-2nd-Offence Team T2","All-Defence Team T1","",""],
-"AccoladesFive":["","","","All-Defence Team T2","","","","All-2nd-Offence Team T2","All-Defence Team T2","All-Defence Team T2","","All-2nd-Offence Team T2","All-2nd-Defence Team T2","Scoring Champ T1","",""],
-"AccoladesSix":["","","","Scoring Champ T2","","","","All-2nd-Defence Team T2","Champion T2","","","All-2nd-Defence Team T2","Champion T2","All-Offence Team T2","",""],
-"AccoladesSeven":["","","","GM","","","","Champion T2","Finals MVP T2","","","","","All-Defence Team T2","",""],
-"AccoladesEight":[" ","","","","","","","","","","","","","Champion T2","",""],
-"History":["Drafted by Choc-Tops","GM of Gentle, Men","Drafted by Choc-Tops","Drafted by Choc-Tops","Drafted by Gentle, Men","Drafted by Gentle, Men","Drafted by Traffic Controllers","GM of Traffic Controllers","Drafted by Traffic Controllers","Drafted by Gentle, Men","GM of Choc-Tops","Drafted by Traffic Controllers","Drafted by Choc-Tops","Drafted by Traffic Controllers","Drafted by Gentle, Men","Drafted by Gentle, Men"],
-"TeamImage":["../Images/CT_Final.png","../Images/GM_Final.png","../Images/CT_Final.png","../Images/CT_Final.png","../Images/GM_Final.png","../Images/GM_Final.png","../Images/TC_Final.png","../Images/TC_Final.png","../Images/TC_Final.png","../Images/GM_Final.png","../Images/CT_Final.png","../Images/TC_Final.png","../Images/CT_Final.png","../Images/TC_Final.png","../Images/GM_Final.png","../Images/GM_Final.png"],
-"PlayerImage":["../Images/Players/Jasper.png","../Images/Players/Conor.png","../Images/Players/Alex.png","../Images/Players/Rudy.png","../Images/Players/Michael.png","../Images/Players/Lukas.png","../Images/Players/SamJ.png","../Images/Players/Clarrie.png","../Images/Players/Kimmy.png","../Images/Players/SamM.png","../Images/Players/Ryan.png","../Images/Players/Nicholas.png","../Images/Players/Chris.png","../Images/Players/Angus.png","../Images/Players/Will.png",""],
+  "Name": ["Jasper Collier", "Conor Farrington", "Alexander Galt", "Rudy Hoschke", "Michael Iffland", "Lukas Johnston", "Sam James", "Clarrie Jones", "William Kim", "Samuel McConaghy", "Ryan Pattemore", "Nicholas Szogi", "Christopher Tomkinson", "Angus Walker", "Will Weekes", "Mitch Yue"],
+  "Team": ["Choc-Tops", "Gentle, Men", "Choc-Tops", "Choc-Tops", "Gentle, Men", "Gentle, Men", "Traffic Controllers", "Traffic Controllers", "Traffic Controllers", "Gentle, Men", "Choc-Tops", "Traffic Controllers", "Choc-Tops", "Traffic Controllers", "Gentle, Men", "Gentle, Men"],
+  "PPG": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "0"],
+  "TP": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "0"],
+  "FPG": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "0"],
+  "TF": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "0"],
+  "MPG": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "0"],
+  "TM": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "0"],
+  "TPG": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "0"],
+  "TT": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "0"],
+  "Missed": [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "0"],
+  "AccoladesOne": ["", "", "All-Defence Team T1", "All-Offence Team T1", "Playmaker T1", "MVP Runner Up T1", "Miles Morales", "LTBO Manager", "MVP T1", "GM", "Perimeter T1", "The Biggest Bird", "MIP T1", "LTBO CEO", "Teammate T1", ""],
+  "AccoladesTwo": ["", "", "Champion T1", "All-Defence Team T1", "Thirdman T1", "All-Offence Team T1", "Champion T2", "Champion T1", "All-Offence Team T1", "All-Offence Team T1", "Champion T1", "Champion T1", "MIP T2", "GM", "Champion T1", ""],
+  "AccoladesThree": ["", "", "All-Offence Team T2", "Champion T1", "All-Offence Team T2", "All-2nd-Offence Team T2", "", "Finals MVP T1", "All-Defence Team T1", "All-Defence Team T1", "", "MVP Runner Up T2", "MVP T2", "All-Offence Team T1", "Teammate T2", ""],
+  "AccoladesFour": ["", "", "All-Defence Team T2", "All-Offence Team T2", "All-2nd-Defence Team T2", "", "", "Fifthman T2", "All-2nd-Offence Team T2", "All-Offence Team T2", "", "X-Factor T2", "All-2nd-Offence Team T2", "All-Defence Team T1", "", ""],
+  "AccoladesFive": ["", "", "", "All-Defence Team T2", "", "", "", "All-2nd-Offence Team T2", "All-Defence Team T2", "All-Defence Team T2", "", "All-2nd-Offence Team T2", "All-2nd-Defence Team T2", "Scoring Champ T1", "", ""],
+  "AccoladesSix": ["", "", "", "Scoring Champ T2", "", "", "", "All-2nd-Defence Team T2", "Champion T2", "", "", "All-2nd-Defence Team T2", "Champion T2", "All-Offence Team T2", "", ""],
+  "AccoladesSeven": ["", "", "", "GM", "", "", "", "Champion T2", "Finals MVP T2", "", "", "", "", "All-Defence Team T2", "", ""],
+  "AccoladesEight": [" ", "", "", "", "", "", "", "", "", "", "", "", "", "Champion T2", "", ""],
+  "History": ["Drafted by Choc-Tops", "GM of Gentle, Men", "Drafted by Choc-Tops", "Drafted by Choc-Tops", "Drafted by Gentle, Men", "Drafted by Gentle, Men", "Drafted by Traffic Controllers", "GM of Traffic Controllers", "Drafted by Traffic Controllers", "Drafted by Gentle, Men", "GM of Choc-Tops", "Drafted by Traffic Controllers", "Drafted by Choc-Tops", "Drafted by Traffic Controllers", "Drafted by Gentle, Men", "Drafted by Gentle, Men"],
+  "TeamImage": ["../Images/CT_Final.png", "../Images/GM_Final.png", "../Images/CT_Final.png", "../Images/CT_Final.png", "../Images/GM_Final.png", "../Images/GM_Final.png", "../Images/TC_Final.png", "../Images/TC_Final.png", "../Images/TC_Final.png", "../Images/GM_Final.png", "../Images/CT_Final.png", "../Images/TC_Final.png", "../Images/CT_Final.png", "../Images/TC_Final.png", "../Images/GM_Final.png", "../Images/GM_Final.png"],
+  "PlayerImage": ["../Images/Players/Jasper.png", "../Images/Players/Conor.png", "../Images/Players/Alex.png", "../Images/Players/Rudy.png", "../Images/Players/Michael.png", "../Images/Players/Lukas.png", "../Images/Players/SamJ.png", "../Images/Players/Clarrie.png", "../Images/Players/Kimmy.png", "../Images/Players/SamM.png", "../Images/Players/Ryan.png", "../Images/Players/Nicholas.png", "../Images/Players/Chris.png", "../Images/Players/Angus.png", "../Images/Players/Will.png", ""],
 
 }
 
 
 Data = [
-  
+]
+
+Missed = [
 ]
 
 /* ------------------------------------------- */
@@ -71,6 +73,14 @@ function getDayArray(index) {
       }
     }
     Points = Finishes + Midranges + ThreePointers + ThreePointers
+    if (Points == 0) {
+      if(Missed[index].Missed[x] == 1) {
+        Points = "Did not Play"
+        Finishes = "Did not Play"
+        Midranges = "Did not Play"
+        ThreePointers = "Did not Play"
+      }
+    }
     DAtoUse.Points.push(Points)
     DAtoUse.Finishes.push(Finishes)
     DAtoUse.Midrange.push(Midranges)
@@ -87,7 +97,7 @@ function getAllDayArrays() {
   for (y = 0; y < Data.length; y++) {
     AllDayArrays.push(getDayArray(y))
   }
-  return(AllDayArrays)
+  return (AllDayArrays)
 }
 
 function getPosition(index, position) {
@@ -99,11 +109,11 @@ function getPosition(index, position) {
   GMWins = 0
   GMLoss = 0
 
-  for (i=0; i<DtoUse.Winner.length; i++) {
+  for (i = 0; i < DtoUse.Winner.length; i++) {
     if (DtoUse.Winner[i] == "Choc-Tops") {
       CTWins += 1
     } else if (DtoUse.Winner[i] == "Traffic Controllers") {
-      TCWins +=1
+      TCWins += 1
     } else {
       GMWins += 1
     }
@@ -111,7 +121,7 @@ function getPosition(index, position) {
     if (DtoUse.Loser[i] == "Choc-Tops") {
       CTLoss += 1
     } else if (DtoUse.Loser[i] == "Traffic Controllers") {
-      TCLoss +=1
+      TCLoss += 1
     } else {
       GMLoss += 1
     }
@@ -127,12 +137,12 @@ function allTeamDBD(team, variable) {
   Wins = []
   Losses = []
   Points = []
-  for (i=0; i<Data.length; i++) {
+  for (i = 0; i < Data.length; i++) {
     LW = 0
     LL = 0
     LP = 0
     Dates.push(Data[i].Date)
-    for (x=0; x<Data[i].Winner.length; x++) {
+    for (x = 0; x < Data[i].Winner.length; x++) {
       if (Data[i].Winner[x] == team) {
         LW += 1
       }
